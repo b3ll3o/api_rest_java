1338227e0124afb115afb0a82a8595eec3e26624
b3ll3o <leonardoandreazzi@gmail.com>
2020-01-23 13:15:24 -0300

    primeiro commit

---
+++ src/main/java/com/produtos/apirest/resources/ProdutoResource.java
@@ -0,0 +1,51 @@
+package com.produtos.apirest.resources;
+
+import com.produtos.apirest.repository.ProdutoRepository;
+
+import java.util.List;
+
+import com.produtos.apirest.models.Produto;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping(value = "/api")
+public class ProdutoResource {
+
+  @Autowired
+  ProdutoRepository produtoRepository;
+
+  @GetMapping("/produtos")
+  public List<Produto> listaProdutos(){
+    return produtoRepository.findAll();
+  }
+
+  @GetMapping("/produto/{id}")
+  public Produto listaProdutoId(@PathVariable(value = "id") long id){
+    return produtoRepository.findById(id);
+  }
+
+  @PostMapping("/produto")
+  public Produto createProduto(@RequestBody Produto produto){
+    return produtoRepository.save(produto);
+  }
+
+  @DeleteMapping("/produto")
+  public void deleteProduto(@RequestBody Produto produto){
+    produtoRepository.delete(produto);
+  }
+
+  @PutMapping("/produto")
+  public Produto updateProduto(@RequestBody Produto produto){
+    return produtoRepository.save(produto);
+  }
+}
